cff2d9b7f4e6378856483b8a129783a57187faec
WissemHajbi <wissemhajbi2002@gmail.com>
2023-04-25 15:29:55 +0100

    Edited

--- teste/views.py
+++ teste/views.py
@@ -3,4 +3,4 @@ from django.shortcuts import render
 
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the polls index.")
+    return HttpResponse("Hello, ahmed. You're at the polls index.")
